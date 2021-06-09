32be161a0bda98bfe3893d9217dd1e6e80171639
Minyoung Shin <mins6649@gmail.com>
2021-06-09 17:31:36 -0400

    nice'

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -7,3 +7,16 @@
 # Define a change method in which to do the migration
 # In this change method, create columns with the correct names and 
 # value types according to the spec
+
+class CreateCostumes < ActiveRecord::Migration[5.2]
+    def change
+        create_table :costumes do |c|
+            c.string :name
+            c.integer :price
+            c.integer :size
+            c.string :image_url
+            c.timestamp :created_at
+            c.timestamp :updated_at
+        end
+    end
+end
