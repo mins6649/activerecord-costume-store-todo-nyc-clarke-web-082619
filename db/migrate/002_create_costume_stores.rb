6424eb2d7f2a1119d05cbd7a7c7ccd962067c637
Minyoung Shin <mins6649@gmail.com>
2021-06-09 17:30:56 -0400

    commit tree 58099affb5cef59f90861b8fc232075ed2f89857

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+    def change
+        create_table :costume_stores do |c|
+            c.string :name
+            c.string :location
+            c.integer :costume_inventory
+            c.integer :num_of_employees
+            c.boolean :still_in_business
+            c.timestamp :opening_time
+            c.timestamp :closing_time
+        end
+    end
+end
